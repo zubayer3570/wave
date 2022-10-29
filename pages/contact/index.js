f70eb3fd35716a391be7927fcd2662f0675ad099
zubayer <zubayer3570@gmail.com>
2022-10-29 19:38:57 +0600

    added router.push

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import Router from 'next/router';
 
 const index = () => {
     // const router = useRouter()
     const sendMail = async (e) => {
+        Router
         e.preventDefault()
         const name = e.target.name.value || "not given"
         const email = e.target.email.value || "not given"
@@ -13,7 +14,7 @@ const index = () => {
         const result = await axios.post("https://mailing-service.onrender.com/sendmail", {
             name, email, location, text
         })
-        // router.push("/")
+        Router.push('/')
     }
     return (
         <>
