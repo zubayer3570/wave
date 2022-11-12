f07f928025fd471a81da325c75d74ebd16636b77
zubayer <zubayer3570@gmail.com>
2022-11-12 15:49:30 +0600

    about done

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,7 +10,7 @@ const index = ({ members }) => {
                 <Navbar />
             </div>
             <p className='text-[#5BCCDE] text-center'>Our Team</p>
-            <p className='font-bold text-[36px] text-[#486A6F] mb-6 text-center'>Let's meet!</p>
+            <p className='font-bold text-[36px] text-[#486A6F] mb-6 text-center'>Let&apos;s meet!</p>
             <div className='grid grid-cols-3 gap-4 mx-[15%] mb-24'>
                 {
                     members?.map((member) => <MemberCard name={member.name} position={member.position} />)
