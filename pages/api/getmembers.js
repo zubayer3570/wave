664bbbf2649dcfd25a86f9afa0131724596a12bc
zubayer <zubayer3570@gmail.com>
2022-11-13 00:02:46 +0600

    test

--- pages/api/getmembers.js
+++ pages/api/getmembers.js
@@ -1,10 +1,11 @@
 import fs from 'fs'
 export default async function getmembers(req, res) {
     try {
-        const data = await fs.promises.readFile("data/members.json", 'utf-8')
-        res.send(data)
+        fs.readFile("data/members.json", 'utf-8', (err, data)=>{
+            res.send(data)
+        })
     } catch (error) {
-        res.send({error, pathRoute: __dirname})
+        res.send(error)
     }
     // res.status(200).json({ name: 'John Doe' })
 }
