8d80336bd4548ff70e621d9ad101151b56079b1f
zubayer <zubayer3570@gmail.com>
2022-11-12 22:59:08 +0600

    hello hi

--- pages/api/getmembers.js
+++ pages/api/getmembers.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'node:fs'
 export default async function handler(req, res) {
     const data = await fs.promises.readFile("public/members.json", 'utf-8')
     res.status(200).json({data: data});
