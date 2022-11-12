a75cb2e0405c3d1074bd53e28e3679e7cf9cb795
zubayer <zubayer3570@gmail.com>
2022-11-12 22:53:09 +0600

    deleted file

--- pages/api/getMembers.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-export default async function handler(req, res) {
-    const data = await fs.promises.readFile("public/members.json", 'utf-8')
-    // res.status(200).json({data: data});
-    res.json({data: "nothing"})
-  }
