ea8a13094bfac7281aca2be048378c2fdc325f82
zubayer <zubayer3570@gmail.com>
2022-11-12 23:06:36 +0600

    added try catch

--- pages/api/getmembers.js
+++ pages/api/getmembers.js
@@ -1,6 +1,10 @@
 import fs from 'node:fs'
 export default async function getmembers(req, res) {
-    const data = await fs.promises.readFile("public/members.json", 'utf-8')
-    res.status(200).json({data: data});
+    try {
+        const data = await fs.promises.readFile("public/members.json", 'utf-8')
+        res.send(data)
+    } catch (error) {
+        res.send(error)
+    }
     // res.status(200).json({ name: 'John Doe' })
 }
