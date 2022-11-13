9ccd0584498df6b57641ede9e083c8b0e258a19a
zubayer <zubayer3570@gmail.com>
2022-11-13 11:17:57 +0600

    test

--- pages/api/getmembers.js
+++ pages/api/getmembers.js
@@ -1,9 +1,8 @@
 import fs from 'fs'
 export default async function getmembers(req, res) {
     try {
-        // const data = await fs.promises.readFile("members.json", 'utf-8')
-        const data = await fs.promises.readdir(`${__dirname}`, 'utf-8')
-        res.send({data, pathname: __dirname})
+        const data = await fs.promises.readFile("data/members.json", 'utf-8')
+        res.send(data)
     } catch (error) {
         res.send(error)
     }
