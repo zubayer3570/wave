2c1538f69bd68bac3ee0ef088ad5f54b94c9c5c4
zubayer <zubayer3570@gmail.com>
2022-11-17 00:18:50 +0600

    hi

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -1,20 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
 const ServiceCard = ({ data }) => {
     return (
-        <div className='flex justify-center' >
-            <div className='w-[300px] rounded overflow-hidden'>
-                <div>
-                    <Image src='/man-photo.jpg' width={300} height={300} />
-                </div>
-                <div className={style.background}>
-                    <div className='pl-2 py-4'>
-                        <p className='font-bold text-[16px] text-[#486A6F]'>{name}</p>
-                        <p className='font-medium text-[16px] text-[#678C92]'>{position}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div></div>
+        // <div className='flex justify-center' >
+        //     <div className='w-[300px] rounded overflow-hidden'>
+        //         <div>
+        //             <Image src='/man-photo.jpg' width={300} height={300} />
+        //         </div>
+        //         <div className={style.background}>
+        //             <div className='pl-2 py-4'>
+        //                 <p className='font-bold text-[16px] text-[#486A6F]'>{name}</p>
+        //                 <p className='font-medium text-[16px] text-[#678C92]'>{position}</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ServiceCard from '../../components/ServiceCard';
-import { services } from '../../data/services';
+// import ServiceCard from '../../components/ServiceCard';
+// import { services } from '../../data/services';
 
 const index = () => {
     return (
         <div>
-            {
+            {/* {
                 services.map(service => <ServiceCard key={service.id} data={service} />)
-            }
+            } */}
         </div>
     );
 };
