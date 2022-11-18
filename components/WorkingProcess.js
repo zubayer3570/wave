16a39ea53397cc836b138fedb83a131a4d89dbcc
zubayer <zubayer3570@gmail.com>
2022-11-18 22:39:10 +0600

    hello

--- components/WorkingProcess.js
+++ components/WorkingProcess.js
@@ -2,21 +2,41 @@ import React from 'react';
 
 const WorkingProcess = () => {
     return (
-        <div>
-            {/* Listen: At first, we listen to all of the clients requirements very carefully.
-
-            Make draft: While listening, we make draft and take notes of important needs of our customers.
-
-            Plan: All of our team member sit together and plan on how our approach will be.
-
-            Build: We create our customer's desired website in this phase.
-
-            Show: After making the whole site, we show it to our clients to make sure everything is okay.
-
-            Revise: After showing to clients, we redesign/ customize the site where needed. And, at the end of this stage, our website gets ready for deliver.
-
-            Deliver: We deliver the complete project after full payment and complete our task. */}
-        </div>
+        <>
+            <div>
+                <p className='text-center text-[#486A6F] font-bold text-[60px] mt-40 mb-8'>How we Work?</p>
+            </div>
+            <div className='mx-[9%]'>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Listening</p>
+                    <p className='font-medium text-center'>Listen: At first, we listen to all of the clients requirements very carefully.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Making Draft</p>
+                    <p className='font-medium text-center'>Make draft: While listening, we make draft and take notes of important needs of our customers.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Plan</p>
+                    <p className='font-medium text-center'>Plan: All of our team member sit together and plan on how our approach will be.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Build</p>
+                    <p className='font-medium text-center'>Build: We create our customer's desired website in this phase.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Show</p>
+                    <p className='font-medium text-center'>Show: After making the whole site, we show it to our clients to make sure everything is okay.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Revise</p>
+                    <p className='font-medium text-center'>Revise: After showing to clients, we redesign/ customize the site where needed. And, at the end of this stage, our website gets ready for deliver.</p>
+                </div>
+                <div className='rounded-2xl shadow-custom-1 my-4 p-6 text-[#486A6F]'>
+                    <p className='font-bold text-[20px] text-center'>Deliver</p>
+                    <p className='font-medium text-center'>Deliver: We deliver the complete project after full payment and complete our task.</p>
+                </div>
+            </div>
+        </>
     );
 };
 
